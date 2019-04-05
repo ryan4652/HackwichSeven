ae91f8fe1f19a771db39afb403f0b21fd71d11b2
Ryan <ryan4652@hawaii.edu>
2019-04-05 12:11:55 -1000

    Added new places of interest

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -28,7 +28,24 @@ centerMapOnLocation(location: initialLocation)
         
 let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude:21.36470, longitude: -158.080098))
         mapView.addAnnotation(restaurantOne)
+        
+let schoolOne = Restaurant(title: "UH West Oahu", type: "School", coordinate: CLLocationCoordinate2D(latitude: 21.3552, longitude: -158.0561))
+        mapView.addAnnotation(schoolOne)
+        
+let airportOne = Restaurant(title: "Kalaeloa Airport", type: "Airport", coordinate: CLLocationCoordinate2D(latitude: 21.3099, longitude: -158.0709))
+        mapView.addAnnotation(airportOne)
     
+let golfCourseOne = Restaurant(title: "Kapolei Golf Course", type: "Golf Course", coordinate: CLLocationCoordinate2D(latitude: 21.3444, longitude: -158.0605))
+        mapView.addAnnotation(golfCourseOne)
+        
+let mallOne = Restaurant(title: "Kamakana Ali'i", type: "Mall", coordinate: CLLocationCoordinate2D(latitude: 21.3337, longitude: -158.0524))
+        mapView.addAnnotation(mallOne)
+        
+let parkOne = Restaurant(title: "Kapolei Regional Park", type: "Park", coordinate: CLLocationCoordinate2D(latitude: 21.3356, longitude: -158.0779))
+        mapView.addAnnotation(parkOne)
+
+        
+        
     }
     
     func centerMapOnLocation(location:CLLocation) {
